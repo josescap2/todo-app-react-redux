f79f74c51ba2096e89283812108121ad20cb5553
Juan Jose Puente Scapolatempo <jjpuentescapolatempo@gmail.com>
2021-01-16 00:35:29 -0300

    Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Made for <a href="https://devchallenges.io/challenges/hH6PbOHBdPm6otzw2De5" target="_blank">DevChallenges</a> by <a href="https://www.linkedin.com/in/josescap/">Puente Scapolatempo, Juan Jose.</a>
+      </p>
+    </footer>
+  );
+}
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import Tabs from './../Tabs/Tabs';
 import Input from './../Input/Input';
 import Todos from './../Todos/Todos';
 import DeleteAll from './../DeleteAll/DeleteAll';
+import Footer from "./../Footer/Footer";
 
 import './../../css/styles.css';
 
@@ -13,6 +14,7 @@ export default function Layout() {
       <Input />
       <Todos />
       <DeleteAll />
+      <Footer />
     </div>
   );
 }
