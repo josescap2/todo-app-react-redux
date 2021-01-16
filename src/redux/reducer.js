9eeca39b1bbd44343a2d3e22344bb601a6669521
Juan Jose Puente Scapolatempo <jjpuentescapolatempo@gmail.com>
2021-01-16 00:06:11 -0300

    finish

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,6 +2,12 @@ const initialState = {
   // 1: All 2: Active 3: Completed
   tab: 1,
 
+  // TODOS
+  // {
+  //   id: Number,
+  //   task: String,
+  //   checked: Boolean
+  // }
   todos: []
 };
 
@@ -19,14 +25,18 @@ function rootReducer(state = initialState, action = null) {
       };
     case 'TOGGLE_CHECK':
       let index = state.todos.findIndex(e => e.id === action.payload);
+
       let todos = [...state.todos];
+
       todos[index].checked = !todos[index].checked
+
       return {
         ...state,
         todos
       };
     case 'DELETE_TODO':
       let filtered = state.todos.filter((e) => e.id !== action.payload);
+      
       return {
         ...state,
         todos: [...filtered]
